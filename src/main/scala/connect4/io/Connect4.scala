34920ee4fa7fc0422962eccc7714e0ade293a767
sam <sam.owen.consultant@springer.com>
2019-06-06 10:18:06 +0100

    further tidy

--- src/main/scala/connect4/io/Connect4.scala
+++ src/main/scala/connect4/io/Connect4.scala
@@ -12,30 +12,29 @@ object Connect4 extends IOApp {
 
   val p1 = Player('X', "Player1")
   val p2 = Player('O', "Player2")
-
   val selectColumn: Regex = "([1-7])".r
 
   def dropCoin(b: Board, currentPlayer: Player, column: String): Either[String, Board] = column match {
-    case selectColumn(c)  => b.play(currentPlayer, c.toInt)
-    case _                => Left(s"Select a column from 1-7: not '$column'")
+    case selectColumn(c) => b.play(currentPlayer, c.toInt)
+    case _ => Left(s"Select a column from 1-7: not '$column'")
+  }
+
+  def eval(command: String, board: Board, p: Player): IO[ExitCode] = IO.suspend {
+    def swapPlayer(p: Player): Player = if (p == p1) p2 else p1
+
+    if (command == null) IO.pure(ExitCode.Success) else dropCoin(board, p, command) match {
+      case Left(err)                          => IO(println(s"Err: $err"))                              *> loop(board, p)
+      case Right(b) if b.gameState == Playing => IO(println(b.draw()))                                  *> loop(b, swapPlayer(p))
+      case Right(b)                           => IO(println(s"Game finished as ${b.gameState} : \n$b")) *> IO.pure(ExitCode.Success)
+    }
   }
 
   def loop(board: Board, p: Player): IO[ExitCode] = IO.suspend {
-
-    def eval(command: String): IO[ExitCode] = {
-      def swapPlayer(p: Player): Player = if (p == p1) p2 else p1
-      if (command == null) IO.pure(ExitCode.Success) else dropCoin(board, p, command) match {
-        case Left(err)                           => IO(println(s"Err: $err"))  *> loop(board, p)
-        case Right(b) if b.gameState == Playing  => IO(println(b.draw()))      *> loop(b, swapPlayer(p))
-        case Right(b)                            => IO(println(s"Game finished as ${b.gameState} : \n$b")) *> IO.pure(ExitCode.Success)
-      }
-    }
-
     for {
-      _     <- IO(print(s"> Choose a column ${p.name}: "))
-      read  <- IO(StdIn.readLine)
-      ec    <- eval(read)
-    } yield ec
+      _           <- IO(print(s"> Choose a column ${p.name}: "))
+      line        <- IO(StdIn.readLine)
+      exitCode    <- eval(line, board, p)
+    } yield exitCode
   }
 
   def run(args: List[String]): IO[ExitCode] =
