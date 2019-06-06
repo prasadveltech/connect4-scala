f6c2a8fff625a31a46cef4b4bc5605e0f5589248
sam <sam.owen.consultant@springer.com>
2019-06-06 10:21:02 +0100

    further tidy

--- src/main/scala/connect4/io/Connect4.scala
+++ src/main/scala/connect4/io/Connect4.scala
@@ -19,9 +19,9 @@ object Connect4 extends IOApp {
     case _ => Left(s"Select a column from 1-7: not '$column'")
   }
 
-  def eval(command: String, board: Board, p: Player): IO[ExitCode] = IO.suspend {
-    def swapPlayer(p: Player): Player = if (p == p1) p2 else p1
+  def swapPlayer(p: Player): Player = if (p == p1) p2 else p1
 
+  def eval(command: String, board: Board, p: Player): IO[ExitCode] = IO.suspend {
     if (command == null) IO.pure(ExitCode.Success) else dropCoin(board, p, command) match {
       case Left(err)                          => IO(println(s"Err: $err"))                              *> loop(board, p)
       case Right(b) if b.gameState == Playing => IO(println(b.draw()))                                  *> loop(b, swapPlayer(p))
@@ -37,11 +37,10 @@ object Connect4 extends IOApp {
     } yield exitCode
   }
 
-  def run(args: List[String]): IO[ExitCode] =
-    loop(new Board(), p1).guaranteeCase {
-      case Canceled =>
-        IO(println("Interrupted: releasing and exiting!"))
-      case _ =>
-        IO(println("Normal exit!"))
-    }
+  def run(args: List[String]): IO[ExitCode] = loop(new Board(), p1).guaranteeCase {
+    case Canceled =>
+      IO(println("Interrupted: releasing and exiting!"))
+    case _ =>
+      IO(println("Normal exit!"))
+  }
 }
